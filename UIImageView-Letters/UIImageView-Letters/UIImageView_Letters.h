6ab72460078599043cee971e320c03aac7ebd05f
lme <laurent.melon@lm2c.be>
2018-07-22 09:37:50 +0200

    Create the xcode projet

---
+++ UIImageView-Letters/UIImageView-Letters/UIImageView_Letters.h
@@ -0,0 +1,19 @@
+//
+//  UIImageView_Letters.h
+//  UIImageView-Letters
+//
+//  Created by Laurent Mélon on 22/07/2018.
+//  Copyright © 2018 LM2C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageView_Letters.
+FOUNDATION_EXPORT double UIImageView_LettersVersionNumber;
+
+//! Project version string for UIImageView_Letters.
+FOUNDATION_EXPORT const unsigned char UIImageView_LettersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageView_Letters/PublicHeader.h>
+
+
